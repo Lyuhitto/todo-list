5785861dfebadfb653f9061cb1706b7a9da1d8f6
Lyuhitto <contact@sujungkim.dev>
2022-11-14 00:14:26 +0900

    Create AddTodo
    Create AddTodo component. This adds items to Todo list.

---
+++ src/component/AddTodo/AddTodo.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+export default function AddTodo({ onAdd }) {
+  const [text, setText] = useState('');
+  const handleChange = (e) => setText(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAdd({id: '고유값', text, status: 'active'})
+  }
+  return (
+    <form
+      onSubmit={handleSubmit}
+    >
+      <input
+        type='text'
+        placeholder='add todo'
+        value={text}
+        onChange={handleChange}
+      />
+      <button type='submit'>Add</button>
+    </form>
+  );
+}
--- src/component/TodoList/TodoList.jsx
+++ src/component/TodoList/TodoList.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
+import AddTodo from './../AddTodo/AddTodo';
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
     { id: '1', text: '장보기', status: 'active' },
     { id: '1', text: '장보기', status: 'active' },
   ]);
+  const handleAdd = (todo) => {
+    setTodos((prev) => ([
+      ...prev,
+      todo
+    ]));
+  }
   return (
     <section>
       <ul>
@@ -12,6 +19,7 @@ export default function TodoList() {
           <li key={item.id}>{item.text}</li>
         ))}
       </ul>
+      <AddTodo onAdd={handleAdd}></AddTodo>
     </section>
   );
 }
