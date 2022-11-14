b069deb74d6484230c5bcebb4ccd60fd70512d83
Lyuhitto <contact@sujungkim.dev>
2022-11-14 16:57:47 +0900

    Add style for AddTodo
    Add style for AddTodo component.

--- src/component/AddTodo/AddTodo.jsx
+++ src/component/AddTodo/AddTodo.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {v4 as uuidv4} from 'uuid';
+import styles from './AddTodo.module.css'
 
 export default function AddTodo({ onAdd }) {
   const [text, setText] = useState('');
@@ -14,15 +15,17 @@ export default function AddTodo({ onAdd }) {
   }
   return (
     <form
+    className={styles.form}
       onSubmit={handleSubmit}
     >
       <input
+      className={styles.input}
         type='text'
         placeholder='add todo'
         value={text}
         onChange={handleChange}
       />
-      <button type='submit'>Add</button>
+      <button type='submit' className={styles.button}>Add</button>
     </form>
   );
 }
