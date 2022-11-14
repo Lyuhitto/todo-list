039196003ecdee4e1fb3c71163ecdc28e759470a
Lyuhitto <contact@sujungkim.dev>
2022-11-14 19:21:41 +0900

    Update TodoItem.jsx

--- src/component/TodoItem/TodoItem.jsx
+++ src/component/TodoItem/TodoItem.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
-import styles from './TodoItem.module.css'
+import styles from './TodoItem.module.css';
 
 export default function TodoItem({ todo, onDelete, onUpdate }) {
   const handleDelete = () => onDelete(todo);
   const handleChange = (e) => {
     const status = e.target.checked ? 'completed' : 'active';
-    onUpdate({...todo, status})
-  }
+    onUpdate({ ...todo, status });
+  };
   const { id, text, status } = todo;
   return (
     <li key={id} className={styles.item}>
       <input
         type='checkbox'
         name='checkbox'
-        id='checkbox'
+        id={id}
         checked={status === 'completed'}
         onChange={handleChange}
         className={styles.checkbox}
       />
-      <label htmlFor='checkbox' className={styles.name}>{text}</label>
+      <label htmlFor={id} className={styles.name}>
+        {text}
+      </label>
       <button type='button' onClick={handleDelete} className={styles.button}>
         <FaTrashAlt className={styles.icon} />
       </button>
