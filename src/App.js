38d500e41411b9927dbec10469df1c2c3e37fcf5
Lyuhitto <contact@sujungkim.dev>
2022-11-14 13:56:16 +0900

    Add styles for whole and container
    Add styles for whole and container.

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ const filters = ['all', 'active', 'completed']
 export default function App() {
   const [filter, setFilter] = useState(filters[0])
   return (
-    <div>
+    <>
       <Header 
         filters={filters}
         filter={filter}
         onFilterChange={(filter) => setFilter(filter)}
       />
       <TodoList filter={filter}/>
-    </div>
+    </>
   );
 }
