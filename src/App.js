2e1d329eadd1c11792ba3de04d39476639dc8dad
Lyuhitto <contact@sujungkim.dev>
2022-11-14 19:04:03 +0900

    Add darkmode feature
    Add darkmode feature.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, {useState} from 'react';
 import Header from './component/Header/Header';
 import TodoList from './component/TodoList/TodoList';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 const filters = ['all', 'active', 'completed']
 
 export default function App() {
   const [filter, setFilter] = useState(filters[0])
   return (
-    <>
+    <DarkModeProvider>
       <Header 
         filters={filters}
         filter={filter}
         onFilterChange={(filter) => setFilter(filter)}
       />
       <TodoList filter={filter}/>
-    </>
+    </DarkModeProvider>
   );
 }
