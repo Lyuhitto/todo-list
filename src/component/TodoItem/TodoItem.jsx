f3560023a8c9593d56ea477b3d469a13b8859c45
Lyuhitto <contact@sujungkim.dev>
2022-11-14 11:19:35 +0900

    Fix typo in TodoItem
    Fix typo in TodoItem.

--- src/component/TodoItem/TodoItem.jsx
+++ src/component/TodoItem/TodoItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
 export default function TodoItem({todo}) {
-  const {key, text, status} = todo
-  return <li key={key}>{text}</li>;
+  const {id, text, status} = todo
+  return <li key={id}>{text}</li>;
 }
