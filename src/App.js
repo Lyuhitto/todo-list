cde4d3dd34ce911881329ac74fe06df869692f6b
Lyuhitto <contact@sujungkim.dev>
2022-11-14 12:02:20 +0900

    Create Header
    Create Header.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './component/Header/Header';
 import TodoList from './component/TodoList/TodoList';
 
 export default function App() {
   return (
     <div>
+      <Header />
       <TodoList />
     </div>
   );
